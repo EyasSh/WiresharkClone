d1cf738ee9282bfc05a5921f1da908479e4caf54
Eyas Sh <eyas.wt@gmail.com>
2024-12-08 11:44:04 +0200

    got some packages ready for project initialization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
+// eslint-disable-next-line no-unused-vars
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import {BrowserRouter as Router, Routes, Route} from 'react-router'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- client/src/Components/Input.jsx
+++ client/src/Components/Input.jsx
@@ -1,12 +1,17 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { useState, useEffect, useCallback } from 'react';
 function Input(props) 
 {
+     
     let [inputType, setInputType] = useState(props.type);
-    let func= props.func;
+    const fn= props?.func;
+    
+   
     return (
         <div>
-            <input type={inputType} onChange={(e) => func(e.target.value)}></input>
+            <input type={inputType} onChange={(e) => fn(e.target.value)}></input>
         </div>
     );
 }
