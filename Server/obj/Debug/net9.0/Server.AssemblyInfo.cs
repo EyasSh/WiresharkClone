9296f0722f1af69cf82831418c50579bf90f01d8
Samer jaraisy <samergeraisy1999@gmail.com>
2024-12-17 14:55:16 +0200

    samer first commit

--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b90e21b7e242a6bc23351e142794a2a1a2ebfa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8c1fd878fbcf15786562c9f65e614425f6ee29f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
