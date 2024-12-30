a64f30bec5f69ac324100920fa72177332dbc804
Eyas Sharary <eyas.wt@gmail.com>
2024-12-30 22:03:42 +0200

    removed extra :root from index.css

--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38b58d2814339e60c2cf8ec98a22b7a73832797f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0eebae8522a19e1202857293c97540ec37b3279")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
