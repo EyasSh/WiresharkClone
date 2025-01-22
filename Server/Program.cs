e7556477b66b74e17e4ceca4a102a55c32c5b985
Samer jaraisy <samergeraisy1999@gmail.com>
2025-01-22 19:57:47 +0200

    added react hosts w/ different ports

--- Server/Program.cs
+++ Server/Program.cs
@@ -75,7 +75,7 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins
         ("http://localhost:5173", "http://localhost:5174",
-         "http://localhost:19006"
+         "http://localhost:19006","http://localhost:5177"
          )
         .WithMethods("GET", "POST", "PUT", "DELETE")
         .WithHeaders(
